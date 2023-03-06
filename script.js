54863b32c72b4119fc3861af4792ea3a8daa2056
AditiGi <aditisingh20144@gmail.com>
2023-03-06 18:14:51 +0530

    map iterations

--- script.js
+++ script.js
@@ -240,3 +240,35 @@ console.log(rest);
 console.log(rest.size); // to check the size
 // rest.clear(); //  to clean a map entirely
 console.log(rest);
+
+// Adding keys to map method other than set (by creating new map)
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again'],
+]);
+console.log(question);
+
+// Convert object to map
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
+
+// Quiz App
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// convert map to array
+console.log([...question]);
