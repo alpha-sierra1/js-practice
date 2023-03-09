33c9de6c7f9c924cedc2882d10284705946f82ae
AditiGi <aditisingh20144@gmail.com>
2023-03-09 22:06:30 +0530

    WIP

--- script.js
+++ script.js
@@ -272,3 +272,23 @@ console.log(question.get(question.get('correct') === answer));
 
 // convert map to array
 console.log([...question]);
+
+// Working with Strings
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log(plane[3]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('Portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
