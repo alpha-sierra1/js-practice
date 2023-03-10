7f535aef337af5917fea024857110a3c6c366b09
AditiGi <aditisingh20144@gmail.com>
2023-03-10 11:30:58 +0530

    slicing

--- script.js
+++ script.js
@@ -292,3 +292,20 @@ console.log(airline.indexOf('Portugal'));
 
 console.log(airline.slice(4));
 console.log(airline.slice(4, 7));
+
+// Not hardcoding the values of string
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('You got the middle seat');
+  else console.log('You got lucky');
+};
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
